8f64938ef8f9028852e3f72d27cc01a427ba8902
boazblake <boazblake@gmail.com>
2016-03-22 20:26:14 -0500

    data is recived and parsed

--- js/app.js
+++ js/app.js
@@ -35,11 +35,95 @@ import fetch from "isomorphic-fetch"
 
 import DOM from 'react-dom'
 import React, {Component} from 'react'
+import Backbone from 'backbone'
 
 function app() {
     // start app
     // new Router()
-    DOM.render(<p>test 2</p>, document.querySelector('.container'))
-}
+    // DOM.render(<p>Boaz</p>, document.querySelector('.container'))
 
+    var GifyView = React.createClass({
+
+
+    	componentWillMount: function(){
+    		var self = this
+    		this.props.gifs.on('sync', function() {self.forceUpdate()})
+    	},
+
+    	render:function(){
+    		console.log('===============rendering app==============')
+    		console.log(this.props.gifs)
+
+    	return (
+    		<div className='gifsContainer'>
+    			<Header/>
+    			<Scroll gifs={this.props.gifs} />
+    		</div>
+    		)
+    	}
+    })
+
+
+    var Header = React.createClass({
+    	render: function() {
+    		return (
+    			console.log('////////scrollFunction//////')
+    			console.log(this)
+
+    			<div className='heading'>
+    				<h1 className='gifTitle'>Your Gifs</h1>
+    				<h3 className='subTitle'>All your gifs in one place! </h3>
+    			</div>
+    			)
+    	}
+    })
+
+    var Scroll = React.createClass({
+    	render: function(){
+    		console.log('////////scrollFunction//////')
+    		console.log(this)
+    		return (
+    			<div></div>
+    			)
+    	}
+    })
+
+
+    var GifyCollection = Backbone.Collection.extend({
+    	url:'http://api.giphy.com/v1/gifs/search?q=husky',
+    	api_key:'dc6zaTOxFJmzC',
+
+    	// parse:function(rawJSON){
+    	// 	console.log('/////parse/////')
+    	// 	console.log(rawJSON)
+    	// }
+    })
+
+
+	var GifyRouter = Backbone.Router.extend({
+		routes:{
+			"detailView/:query":"_detailView",
+			"gallery/:query": "_galleryView",
+			"*home":"_galleryView"
+		},
+
+		_galleryView: function(query) {
+
+			var coll = new GifyCollection()
+			coll.fetch({
+				data: {
+					q: query,
+					api_key: coll.api_key
+				}
+			})
+			DOM.render(<GifyView gifs={coll}/>, document.querySelector('.container'))
+		},
+
+		initialize: function(){
+			Backbone.history.start()
+		}
+	})
+
+	var rtr = new GifyRouter()
+}
 app()
